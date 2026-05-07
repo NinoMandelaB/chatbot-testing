efae6f6a998c146e2e25612fc841bdfe3352d77c
NinoMandelaB <nino.bachmann@pm.me>
2026-05-07 08:32:27 +0200

    Update extra_body to use chat_template_kwargs

--- app.py
+++ app.py
@@ -812,7 +812,7 @@ def chat():
             messages=messages,
             max_tokens=1024,
             stream=False,
-            extra_body={"enable_thinking": False},
+            extra_body={"chat_template_kwargs": {"enable_thinking": False} },
         )
 
         reply = ""
